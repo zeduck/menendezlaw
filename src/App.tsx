8e2445e465d48e1a3a94a34cb30a2e7449264d17
Roy Zhang <roysmiley.ca@gmail.com>
2023-11-10 21:17:17 -0500

    tailwind and material setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './App.css'
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig: { [id: string]: string | undefined } = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 export default function App() {
@@ -18,7 +18,7 @@ export default function App() {
   return (
     <>
 
-      <h1>Mendendez Law</h1>
+      <h1 className="text-3xl font-bold underline">Mendendez Law</h1>
     </>
   );
 }
