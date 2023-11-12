47d2cffd343748f13f815d5ca8126f90b699ed03
Roy Zhang <roysmiley.ca@gmail.com>
2023-11-11 19:20:50 -0500

    hmm

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Section from './components/Section';
-import { useInView, animated } from '@react-spring/web';
+import { useInView, animated, SpringValue } from '@react-spring/web';
 import { useRef } from 'react';
 
 const firebaseConfig: { [id: string]: string | undefined } = {
@@ -26,7 +26,7 @@ export default function App() {
   const contact = useRef(null!);
 
   const messageFade = {
-    from: { opacity: 0, y: -250, },
+    from: { opacity: 0, y: 250, },
     to: { opacity: 1, y: 0, },
   }
 
@@ -34,35 +34,37 @@ export default function App() {
   const [locationTrigger, locationMessage] = useInView(() => (messageFade), { rootMargin: '-49% 0px', })
   const [aboutusTrigger, aboutusMessage] = useInView(() => (messageFade), { rootMargin: '-49% 0px', })
   const [contactTrigger, contactMessage] = useInView(() => (messageFade), { rootMargin: '-49% 0px', })
+
+  const messages: { 
+    style: {
+      opacity: SpringValue<number>;
+      y: SpringValue<number>;
+    }, 
+    message: string 
+  }[] = [
+    {style: serviceMessage, message: 'Injustice anywhere is a threat to justice everywhere. As a refugee lawyer, I help people fleeing from persecution find peace and security in Canada.'},
+    {style: locationMessage, message: 'My own family immigrated to Canada from Colombia. Being a newcomer here instilled in me a passion for others who brave everything to find happiness in a strange land.'},
+    {style: aboutusMessage, message: 'If you are looking for a tireless and compassionate advocate for your refugee claim, reach out to me! I can speak Spanish and English.'},
+    {style: contactMessage, message: 'My immigration firm offers help completing refugee claims and visitor visa extensions. Located right in the nation”s capital, we can work virtually or in person. I look forward to hearing from you.'},
+  ];
   
   return (
     <div className='font-sans text-primary_content'>
+
+      {/* POP UP MESSAGES */}
+      { 
+        messages.map(({ style, message }) => {
+          return (
+            <animated.div className="fixed z-50 right-12 bottom-12" style={style}> 
+              <div className='bg-secondary text-secondary_content rounded-[3rem] relative py-6 px-8 ml-12 text-center md:max-w-4xl'>
+                <p className='relative text-xl md:text-2xl text-center'> { message } </p> 
+              </div>
+            </animated.div>
+          );
+        }) 
+      }
       
-      <animated.div className="fixed z-50 left-1/2 top-12 " style={serviceMessage}> 
-        <div className='-translate-x-1/2 bg-secondary text-secondary_content rounded-[3rem] relative p-12 text-center'>
-          <p className='relative text-5xl text-center'>ONE</p> 
-        </div>
-      </animated.div>
-
-      <animated.div className="fixed z-50 left-1/2 top-12 " style={locationMessage}> 
-        <div className='-translate-x-1/2 bg-secondary text-secondary_content rounded-[3rem] relative p-12 text-center'>
-          <p className='relative text-5xl text-center'>TWO</p> 
-        </div>
-      </animated.div>
-
-      <animated.div className="fixed z-50 left-1/2 top-12 " style={aboutusMessage}> 
-        <div className='-translate-x-1/2 bg-secondary text-secondary_content rounded-[3rem] relative p-12 text-center'>
-          <p className='relative text-5xl text-center'>THREE</p> 
-        </div>
-      </animated.div>
-
-      <animated.div className="fixed z-50 left-1/2 top-12 " style={contactMessage}> 
-        <div className='-translate-x-1/2 bg-secondary text-secondary_content rounded-[3rem] relative p-12 text-center'>
-          <p className='relative text-5xl text-center'>FOUR</p> 
-        </div>
-      </animated.div>
-
-      
+      {/* CONTENT */}
       <Hero/>
 
       <div ref={servicesTrigger} className='w-full text-center'>
