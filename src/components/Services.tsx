12f76285f4f33562b9b1550ed9ce7973c6164e7b
Roy Zhang <roysmiley.ca@gmail.com>
2023-11-12 12:10:57 -0500

    good

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,17 +1,17 @@
 export default function Services() {
   const cards: { 
     title: string, 
-    body: string
+    // body: string
   }[] = [
     {
       title: 'Refugee claims',
-      body: ''
+      // body: ''
     }, {
       title: 'Visitor visa extensions',
-      body: ''
+      // body: ''
     }, {
       title: 'Humanitarian & compassionate ground applications',
-      body: ''
+      // body: ''
     }
   ];
 
@@ -20,7 +20,7 @@ export default function Services() {
         <h1 className="text-5xl md:text-6xl pb-8 md:pb-12"> Services </h1>
         <div className="flex flex-col justify-between gap-4 md:gap-6">
           {
-            cards.map(({ title, body }) => {
+            cards.map(({ title}) => {
               return (
                 <div className="bg-primary text-primary_content rounded-3xl w-full p-6 md:p-12 hover:-translate-y-1 hover:bg-primary_focus transition-all break-words">
                   <h1 className="text-2xl lg:text-3xl"> { title } </h1>
