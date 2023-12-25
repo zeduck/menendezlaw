a4261f128878a958e7020878762eb79c2b310755
Roy Zhang <roysmiley.ca@gmail.com>
2023-12-25 00:45:03 -0500

    padding

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -44,22 +44,22 @@ export default function Services() {
         <p className="text-xl md:text-2xl pb-8 md:pb-12"> Injustice anywhere is a threat to justice everywhere. As a refugee lawyer, I help people fleeing from persecution to find peace and security in Canada. I accept legal aid certificates and private clients.</p>
         <div className="flex flex-col justify-between gap-4 md:gap-6">
           
-          <div onClick={toggleVisible1} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible1 && 'max-h-24'} ${visible1 && 'max-h-64'} transition-all`}>
+          <div onClick={toggleVisible1} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible1 && 'max-h-24 md:max-h-[94px] lg:max-h-24'} ${visible1 && 'max-h-64'} transition-all`}>
             <h1 className="text-2xl lg:text-3xl"> { cards[0].title } </h1>
             <p className="text-lg md:text-xl pt-4 md:pt-6"> { cards[0].body } </p>
           </div>
           
-          <div onClick={toggleVisible2} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible2 && 'max-h-24'} ${visible2 && 'max-h-64'} transition-all`}>
+          <div onClick={toggleVisible2} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible2 && 'max-h-16 md:max-h-[94px] lg:max-h-24'} ${visible2 && 'max-h-64'} transition-all`}>
             <h1 className="text-2xl lg:text-3xl"> { cards[1].title } </h1>
             <p className="text-lg md:text-xl pt-4 md:pt-6"> { cards[1].body } </p>
           </div>
           
-          <div onClick={toggleVisible3} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible3 && 'max-h-24'} ${visible3 && 'max-h-64'} transition-all`}>
+          <div onClick={toggleVisible3} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible3 && 'max-h-24 md:max-h-[94px] lg:max-h-24'} ${visible3 && 'max-h-64'} transition-all`}>
             <h1 className="text-2xl lg:text-3xl"> { cards[2].title } </h1>
             <p className="text-lg md:text-xl pt-4 md:pt-6"> { cards[2].body } </p>
           </div>
 
-          <div onClick={toggleVisible4} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible4 && 'max-h-24'} ${visible4 && 'max-h-64'} transition-all`}>
+          <div onClick={toggleVisible4} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible4 && 'max-h-16 md:max-h-[94px] lg:max-h-24'} ${visible4 && 'max-h-64'} transition-all`}>
             <h1 className="text-2xl lg:text-3xl"> { cards[3].title } </h1>
             <p className="text-lg md:text-xl pt-4 md:pt-6"> { cards[3].body } </p>
           </div>
