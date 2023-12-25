e639a528607d1a12b9aa2ec0138ec5b3883b25f3
Roy Zhang <roysmiley.ca@gmail.com>
2023-12-25 01:38:14 -0500

    optimize services dropdowns

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -13,7 +13,7 @@ export default function Services() {
       body: 'I can help you prepare an application to prolong your visitor status here in Canada.'
     }, {
       title: 'Humanitarian & compassionate ground applications',
-      body: 'And for humanitarian applications - I regularly prepare applications for permanent residence on humanitarian grounds (“H&C Applications”). These are paper-based applications made to Immigration, Refugees and Citizenship Canada (IRCC).'
+      body: 'For humanitarian applications - I regularly prepare applications for permanent residence on humanitarian grounds (“H&C Applications”). These are paper-based applications made to Immigration, Refugees and Citizenship Canada (IRCC).'
     }, {
       title: 'Permanent residency applications',
       body: ''
@@ -25,43 +25,30 @@ export default function Services() {
   const [ visible3, setVisible3 ] = useState(false);
   const [ visible4, setVisible4 ] = useState(false);
 
-  const toggleVisible1 = () => {
-    setVisible1(!visible1);
-  };
-  const toggleVisible2 = () => {
-    setVisible2(!visible2);
-  };
-  const toggleVisible3 = () => {
-    setVisible3(!visible3);
-  };
-  const toggleVisible4 = () => {
-    setVisible4(!visible4);
-  };
-
   return (
       <div className="m-12 md:m-16">
         <h1 className="text-5xl md:text-6xl pb-6 md:pb-8"> Services </h1>
         <p className="text-xl md:text-2xl pb-8 md:pb-12"> Injustice anywhere is a threat to justice everywhere. As a refugee lawyer, I help people fleeing from persecution to find peace and security in Canada. I accept legal aid certificates and private clients.</p>
         <div className="flex flex-col justify-between gap-4 md:gap-6">
           
-          <div onClick={toggleVisible1} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible1 && 'max-h-24 md:max-h-[94px] lg:max-h-24'} ${visible1 && 'max-h-64'} transition-all`}>
+          <div onClick={() => setVisible1(!visible1)} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden transition-all`}>
             <h1 className="text-2xl lg:text-3xl"> { cards[0].title } </h1>
-            <p className="text-lg md:text-xl pt-4 md:pt-6"> { cards[0].body } </p>
+            <p className={`text-lg md:text-xl pt-4 md:pt-6 transition-all ${!visible1 && 'hidden'} ${visible1 && 'grid'}`}> { cards[0].body } </p>
           </div>
           
-          <div onClick={toggleVisible2} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible2 && 'max-h-16 md:max-h-[94px] lg:max-h-24'} ${visible2 && 'max-h-64'} transition-all`}>
+          <div onClick={() => setVisible2(!visible2)} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden transition-all`}>
             <h1 className="text-2xl lg:text-3xl"> { cards[1].title } </h1>
-            <p className="text-lg md:text-xl pt-4 md:pt-6"> { cards[1].body } </p>
+            <p className={`text-lg md:text-xl pt-4 md:pt-6 transition-all ${!visible2 && 'hidden'} ${visible2 && 'grid'}`}> { cards[1].body } </p>
           </div>
           
-          <div onClick={toggleVisible3} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible3 && 'max-h-24 md:max-h-[94px] lg:max-h-24'} ${visible3 && 'max-h-64'} transition-all`}>
+          <div onClick={() => setVisible3(!visible3)} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden transition-all`}>
             <h1 className="text-2xl lg:text-3xl"> { cards[2].title } </h1>
-            <p className="text-lg md:text-xl pt-4 md:pt-6"> { cards[2].body } </p>
+            <p className={`text-lg md:text-xl pt-4 md:pt-6 transition-all ${!visible3 && 'hidden'} ${visible3 && 'grid'}`}> { cards[2].body } </p>
           </div>
 
-          <div onClick={toggleVisible4} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden ${!visible4 && 'max-h-16 md:max-h-[94px] lg:max-h-24'} ${visible4 && 'max-h-64'} transition-all`}>
+          <div onClick={() => setVisible4(!visible4)} className={`bg-primary text-primary_content rounded-3xl w-full p-4 md:p-8 hover:-translate-y-1 hover:bg-primary_focus hover:cursor-pointer break-words overflow-hidden transition-all`}>
             <h1 className="text-2xl lg:text-3xl"> { cards[3].title } </h1>
-            <p className="text-lg md:text-xl pt-4 md:pt-6"> { cards[3].body } </p>
+            <p className={`text-lg md:text-xl pt-4 md:pt-6 transition-all ${!visible4 && 'hidden'} ${visible4 && 'grid'}`}> { cards[3].body } </p>
           </div>
           
           {/* {
