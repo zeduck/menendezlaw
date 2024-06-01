bec300ec503ddb3623e8beae4bb6beb5ebf0ceda
Roy Zhang <roysmiley.ca@gmail.com>
2024-06-01 00:43:32 -0400

    optimzie hero bg image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,10 @@ export default function Hero() {
   return (
     <div className="m-3 md:m-6 mb-12 md:mb-24 border-2 border-transparent relative overflow-hidden rounded-[3rem]">
       
-      <div className="bg-[url('/bricks.jpg')] absolute w-full h-full brightness-[.6] bg-center bg-cover saturate-50 blur-[1px]"/>
+      <div 
+        className="absolute w-full h-full brightness-[.6] bg-center bg-cover saturate-50 blur-[1px]"
+        style={{ backgroundImage: "url('/bricks.jpg')" }}
+      />
       
       <div className="my-36 md:my-48 xl:my-64 text-center relative z-10">
         <h1 className="text-6xl md:text-8xl lg:text-[8rem] pb-8 md:pb-12 font-medium">Menendez Law</h1>
